1e95c98f2e8a9a4e7c7a0dcccce76473aca696fc
EC2 Default User <ec2-user@ip-172-31-10-223.ap-northeast-1.compute.internal>
2025-07-03 06:38:00 +0000

    Deploy_0703_2

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,8 +2,6 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   
   def after_sign_up_path_for(resource)
     current_user
